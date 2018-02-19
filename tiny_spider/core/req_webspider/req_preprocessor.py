0a36fa9f317aa2f61aca2c57af7cbf095b9aba11
Jgirl033 <841917374@qq.com>
2018-02-19 22:32:40 +0800

    Added to pre-process requests. 1. Added thread. 2. Unfinished request prepare.

--- tiny_spider/core/req_webspider/req_preprocessor.py
+++ tiny_spider/core/req_webspider/req_preprocessor.py
@@ -1,16 +1,30 @@
-import json
 import threading
+
+import time
 
 from tiny_spider.base.common import Global
 from tiny_spider.base.decorator import singleton
 from tiny_spider.core.queue_manager import QueueManager
-from tiny_spider.net.tcp_manager import TCPManager
 
 
 @singleton
 class ReqPreprocessor(threading.Thread):
+    def __new__(cls, *args, **kwargs):
+        q = QueueManager()
+        cls.__req_preparing_queue = q.get(Global.get_req_preparing_type())
+        cls.__req_crawling_queue = q.get(Global.get_req_crawling_type())
+        return object.__new__(cls)
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.__preparing_queue = self.__req_preparing_queue.queue
+        self.__crawling_queue = self.__req_crawling_queue.queue
+
+    def run(self):
+        while True:
+            self.prepare_request()
+            time.sleep(3)
 
     def prepare_request(self):
-        while True:
-            dict_req = self.__req_queue.get()
-            print(dict_req)
+        dict_req = self.__preparing_queue.get()
+        self.__crawling_queue.put(dict_req)
