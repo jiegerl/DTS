2bfd52746082a08d5237b91ba24e18a2ca4816e6
Jgirl033 <841917374@qq.com>
2018-02-21 00:02:22 +0800

    Added request data process.

--- tiny_spider/core/req_webspider/req_recevier.py
+++ tiny_spider/core/req_webspider/req_recevier.py
@@ -5,8 +5,6 @@ import threading
 from tiny_spider.base.common import Global
 from tiny_spider.base.decorator import singleton
 from tiny_spider.core.queue_manager import QueueManager
-from tiny_spider.core.req_webspider.msg_processor import MsgProcessor
-from tiny_spider.model.task import Request
 from tiny_spider.net.tcp_manager import TCPManager
 
 
@@ -34,20 +32,6 @@ class ReqReceiver(threading.Thread):
         json_req = sock.recv(1024)
         if not json_req:
             logging.info("received empty request %s" % json_req)
-        else:
-            dict_req = json.loads(json_req.decode('utf8'))
-            obj_req = Request()
-            obj_req.task_id = dict_req['task_id']
-            obj_req.req_id = dict_req['req_id']
-            obj_req.req_type = dict_req['req_type']
-            obj_req.req_status = Global.get_req_crawling_type()
-            obj_req.urls_count = dict_req['urls_count']
-            obj_req.urls_args = dict_req['urls_args']
-            self.__req_queue.put(obj_req)
-            dict_msg = dict()
-            dict_msg['task_id'] = obj_req.task_id
-            dict_msg['req_id'] = obj_req.req_id
-            dict_msg['req_status'] = obj_req.req_status
-            json_msg = json.dumps(dict_msg)
-            sock.send(json_msg)
-            logging.info("received request %s from %s" % (dict_req, addr))
+        dict_req = json.loads(json_req.decode('utf8'))
+        self.__req_queue.put(dict_req)
+        logging.info("received request %s from %s" % (dict_req, addr))
