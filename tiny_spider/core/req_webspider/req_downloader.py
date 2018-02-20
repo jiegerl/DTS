a9235f133785b9473cbbb65053642c8445335db9
Jgirl033 <841917374@qq.com>
2018-02-20 23:39:33 +0800

    Added response message to scheduler when finished request.

--- tiny_spider/core/req_webspider/req_downloader.py
+++ tiny_spider/core/req_webspider/req_downloader.py
@@ -6,6 +6,8 @@ from tiny_spider.apps.cbo.movie_discoverer import MovieDiscoverer
 from tiny_spider.base.common import Global
 from tiny_spider.base.decorator import singleton
 from tiny_spider.core.queue_manager import QueueManager
+from tiny_spider.core.req_webspider.msg_processor import MsgProcessor
+from tiny_spider.model.task import Response
 
 
 @singleton
@@ -29,22 +31,30 @@ class ReqDownloader(threading.Thread):
         S.download_page() -> int
 
         Download html page according to type of source page.
-        :return:negative number presents request data or system initial error;
-                non negative number presents the count of fail urls.
+        :return: negative number presents request data or system initial error;
+                 non negative number presents the count of fail urls.
         """
-        dict_req = self.__req_queue.get()
-        if 'req_type' in dict_req.keys():
-            list_fail_urls = list()
-            if dict_req['req_type'] == '0':  # cbo movie discoverer
-                cbo_discoverer = MovieDiscoverer()
-                res = cbo_discoverer.init_sys("conf/config.ini")
-                if res == -1:
-                    return -1
-                list_urls = cbo_discoverer.load_req(dict_req['urls_args'])
-                list_fail_urls = cbo_discoverer.exec_spider(list_urls)
-            return len(list_fail_urls)
-        else:
-            return -1
+        obj_req = self.__req_queue.get()
+        list_urls = list()
+        list_fail_urls = list()
+        if obj_req.req_type == '0':  # cbo movie discoverer
+            cbo_discoverer = MovieDiscoverer()
+            res = cbo_discoverer.init_sys("conf/config.ini")
+            if res == -1:
+                return -1
+            list_urls = cbo_discoverer.load_req(obj_req.urls_args)
+            list_fail_urls = cbo_discoverer.exec_spider(list_urls)
+        pages_count = len(list_urls) - len(list_fail_urls)
+        obj_res = Response()
+        obj_res.task_id = obj_req.task_id
+        obj_res.req_id = obj_req.req_id
+        obj_res.req_status = Global.get_res_crawled_type()
+        obj_res.pages_count = pages_count
+        obj_res.pages_args = dict()
+        mp = MsgProcessor()
+        mp.send_res_msg(obj_res)
+        return len(list_fail_urls)
+
 
 
 
