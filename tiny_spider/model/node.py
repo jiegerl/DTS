42fdc2ec2f8e42bad7950814c0401bb373e3665d
Jgirl033 <841917374@qq.com>
2018-02-21 00:06:32 +0800

    Added node classes.

--- tiny_spider/model/node.py
+++ tiny_spider/model/node.py
@@ -1,13 +1,32 @@
-class SchedulerNode:
-    def __init__(self):
-        pass
+class Node:
+    def __init__(self, node_ip, node_type, node_status):
+        self.__node_ip = node_ip
+        self.__node_type = node_type
+        self.__node_status = node_status
+
+    @property
+    def node_ip(self):
+        return self.__node_ip
+
+    @property
+    def node_type(self):
+        return self.__node_type
+
+    @property
+    def node_status(self):
+        return self.__node_status
 
 
-class SpiderNode:
-    def __init__(self):
-        pass
+class SchedulerNode(Node):
+    def __init__(self, node_ip, node_status, node_type='s'):
+        super().__init__(node_ip, node_type, node_status)
 
 
-class ManagerNode:
-    def __init__(self):
-        pass
+class WebSpiderNode(Node):
+    def __init__(self, node_ip, node_status, node_type='w'):
+        super().__init__(node_ip, node_type, node_status)
+
+
+class ManagerNode(Node):
+    def __init__(self, node_ip, node_status, node_type='m'):
+        super().__init__(node_ip, node_type, node_status)
