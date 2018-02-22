050af4c449052f4076eadd95c349440049beafb7
Jgirl033 <841917374@qq.com>
2018-02-23 00:14:14 +0800

    Changed cmd connection.

--- tiny_spider/net/tcp_manager.py
+++ tiny_spider/net/tcp_manager.py
@@ -20,7 +20,6 @@ class TCPManager:
 
     def set_cmd_connect(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((self.__schedule_ip, self.__submit_port))
         s.listen(5)
         return s
