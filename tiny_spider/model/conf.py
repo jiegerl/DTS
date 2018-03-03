3109b0658c9af20e9960eb6061ab1323b60989ef
Jgirl033 <841917374@qq.com>
2018-03-03 23:58:36 +0800

    Added set method.

--- tiny_spider/model/conf.py
+++ tiny_spider/model/conf.py
@@ -15,10 +15,22 @@ class Configure:
     def dispatch_port(self):
         return self.__dispatch_port
 
+    @dispatch_port.setter
+    def dispatch_port(self, dispatch_port):
+        self.__dispatch_port = dispatch_port
+
     @property
     def reduce_port(self):
         return self.__reduce_port
 
+    @reduce_port.setter
+    def reduce_port(self, reduce_port):
+        self.__reduce_port = reduce_port
+
     @property
     def message_port(self):
         return self.__message_port
+
+    @message_port.setter
+    def reduce_port(self, message_port):
+        self.__message_port = message_port
