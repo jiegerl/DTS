1880800a67bc0db3f3f2826f5dd4baa614d88f1a
Jgirl033 <841917374@qq.com>
2018-03-04 23:12:16 +0800

    Refactored.

--- tiny_spider/model/node.py
+++ tiny_spider/model/node.py
@@ -1,32 +1,44 @@
-class Node:
-    def __init__(self, node_ip, node_type, node_status):
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: Jgirl
+@contact: 841917374@qq.com
+@software: pycharm
+@file: node.py
+@time: 2018/3/4/004 21:45
+@desc:
+"""
+
+
+class LocalNode:
+
+    def __init__(self, node_ip, node_status):
         self.__node_ip = node_ip
-        self.__node_type = node_type
         self.__node_status = node_status
 
     @property
     def node_ip(self):
         return self.__node_ip
 
-    @property
-    def node_type(self):
-        return self.__node_type
+    @node_ip.setter
+    def node_ip(self, node_ip):
+        self.__node_ip = node_ip
 
     @property
     def node_status(self):
         return self.__node_status
 
-
-class SchedulerNode(Node):
-    def __init__(self, node_ip, node_status, node_type='s'):
-        super().__init__(node_ip, node_type, node_status)
-
-
-class WebSpiderNode(Node):
-    def __init__(self, node_ip, node_status, node_type='w'):
-        super().__init__(node_ip, node_type, node_status)
+    @node_status.setter
+    def node_status(self, node_status):
+        self.__node_status = node_status
 
 
-class ManagerNode(Node):
-    def __init__(self, node_ip, node_status, node_type='m'):
-        super().__init__(node_ip, node_type, node_status)
+class SchedulerNode(LocalNode):
+
+    def __init__(self, node_ip, node_status):
+        super(SchedulerNode, self).__init__(node_ip, node_status)
+
+
+class WebSpiderNode(LocalNode):
+    def __init__(self, node_ip, node_status):
+        super(WebSpiderNode, self).__init__(node_ip, node_status)
