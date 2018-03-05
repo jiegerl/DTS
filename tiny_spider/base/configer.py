ff0302f0a1d51fe0e934953ba64e80189ff2630b
Jgirl033 <841917374@qq.com>
2018-03-05 22:43:04 +0800

    Added configure apply.

--- tiny_spider/base/configer.py
+++ tiny_spider/base/configer.py
@@ -1,22 +1,31 @@
+import configparser
+
+from tiny_spider.base.decorator import singleton
+from tiny_spider.model.conf import Configure
+
+
+@singleton
 class Configer:
-    def __init__(self, conf_path):
-        self.__conf_path = conf_path
+    def __new__(cls, conf_path):
+        cls.__conf_path = conf_path
+        cls.__conf_obj = Configure()
+        return object.__new__(cls)
 
     @property
     def conf_path(self):
         return self.__conf_path
 
-    @conf_path.setter
-    def conf_path(self, conf_path):
-        if isinstance(conf_path, str):
-            self.__conf_path = conf_path
-        else:
-            print("Type invalid!!!")
-            raise Exception
+    @property
+    def conf_obj(self):
+        return self.__conf_obj
 
-
-if __name__ == '__main__':
-    c = Configer("jie")
-    print(c.conf_path)
-    c.conf_path = ("ming")
-    print(c.conf_path)
+    def apply_conf(self):
+        conf = configparser.ConfigParser()
+        conf.read(self.__conf_path)
+        conf_obj = self.conf_obj
+        conf_obj.sdl_ip = conf.get('ip', 'sdl_ip')
+        conf_obj.cmd_port = conf.get('port', 'cmd_port')
+        conf_obj.dsp_port = conf.get('port', 'dsp_port')
+        conf_obj.msg_port = conf.get('port', 'msg_port')
+        conf_obj.rdc_port = conf.get('port', 'rdc_port')
+        return 0
