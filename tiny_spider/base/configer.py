331f892d0dff894dbf090c2d9b268509f54e309f
Jgirl033 <841917374@qq.com>
2018-03-06 22:40:30 +0800

    Added check configure value.

--- tiny_spider/base/configer.py
+++ tiny_spider/base/configer.py
@@ -13,7 +13,7 @@ class Configer:
         return object.__new__(cls)
 
     def __init__(self):
-        pu = PathUtils(os.getcwd())
+        pu = PathUtils()
         conf_path = pu.conf_path
         self.__conf_path = conf_path
         self.__conf_obj = Configure()
@@ -22,13 +22,28 @@ class Configer:
     def conf_obj(self):
         return self.__conf_obj
 
+    def check_conf(self):
+        conf_obj = self.__conf_obj
+        # check if IP is valid
+        pass
+        # check if PORT is valid
+        if not conf_obj.dsp_port.isdigit():
+            return 1
+        if not conf_obj.cmd_port.isdigit():
+            return 2
+        if not conf_obj.msg_port.isdigit():
+            return 3
+        if not conf_obj.rdc_port.isdigit():
+            return 4
+        return 0
+
     def apply_conf(self):
         conf = configparser.ConfigParser()
         conf.read(self.__conf_path)
         conf_obj = self.conf_obj
-        conf_obj.sdl_ip = conf.get('ip', 'sdl_ip')
-        conf_obj.cmd_port = conf.get('port', 'cmd_port')
-        conf_obj.dsp_port = conf.get('port', 'dsp_port')
-        conf_obj.msg_port = conf.get('port', 'msg_port')
-        conf_obj.rdc_port = conf.get('port', 'rdc_port')
+        conf_obj.sdl_ip = conf.get('IP', 'SDL_IP')
+        conf_obj.cmd_port = conf.get('PORT', 'CMD_port')
+        conf_obj.dsp_port = conf.get('PORT', 'DSP_PORT')
+        conf_obj.msg_port = conf.get('PORT', 'MSG_PORT')
+        conf_obj.rdc_port = conf.get('PORT', 'RDC_PORT')
         return 0
